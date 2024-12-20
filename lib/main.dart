85986dde6f903b378b4aa455e39dfcece87abb8f
yasmishabana <shabana1.yasmi@gmail.com>
2024-12-20 14:57:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:pose_detection_app/pose_detector_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  runApp(PoseDetectionApp(cameras: cameras));
+}
+
+class PoseDetectionApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  PoseDetectionApp({required this.cameras});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PoseDetectorView(),
+    );
+  }
+}
+
+
